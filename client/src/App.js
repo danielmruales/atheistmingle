a013c7ec410cbbb43b1c083e92a97da081aeb54d
Daniel Ruales <danielmruales@gmail.com>
2019-04-23 18:38:12 -0600

    centered card in it's own div

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 import Card from './Card/Card.js'
 import Matches from './Matches/Matches.js'
-import Split from 'react-split'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Athiest Mingle</h1>
-        <h2>Find Other Non-believers!</h2>
-        {/* <Split sizes={[75,25]}> */}
-          {/* <div className='container'> */}
-            <Card/>
-            <Matches/>
-          {/* </div> */}
-        {/* </Split> */}
+            <div id='mainDiv'>
+              <Card/>
+              <Matches/>
+            </div>
       </div>
     );
   }
--- client/src/Card/Card.js
+++ client/src/Card/Card.js
@@ -4,7 +4,10 @@ import './Card.css'
 class Card extends Component {
     render() {
         return (
+            
             <div className='mainCardDiv'>
+                <h1 className='title'>Athiest Mingle</h1>
+                <h2 className='subtitle'>Find Other Non-believers!</h2>
                 <div className='cardImage'>
                     <img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60' alt='user profile' className='image' />
                 </div>
