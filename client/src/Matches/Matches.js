61ee0a0730e952bba765d3b95ff8fea5fbbeb664
Daniel Ruales <danielmruales@gmail.com>
2019-05-04 16:09:17 -0600

    added mock profile pic and made changes to matches section

--- client/src/Matches/Matches.js
+++ client/src/Matches/Matches.js
@@ -5,11 +5,11 @@ class Matches extends Component {
         return (
             <div className='mainMatchesDiv'>
                 <div className='matchesNav'>
-                    <img src='' alt='profile'/>
+                    <img src='https://images.unsplash.com/photo-1548063111-005357d141d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' alt='profile'/>
                     <h1> My Profile</h1>
                 </div>
                 <div className='matches'>
-                    <div className='options'><p>Matches</p></div>
+                    <div className='options'><p>Matches</p> <p>Messages</p></div>
                     <div className='imgDiv'>
                         <img src='https://images.unsplash.com/photo-1553564003-28e601062b3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80' alt='matches'/>
                         <img src='https://images.unsplash.com/photo-1553564003-28e601062b3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80' alt='matches'/>
