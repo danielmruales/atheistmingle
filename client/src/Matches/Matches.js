e3a568db8f214f6fa3a5f91d72b4e70d693ac708
Daniel Ruales <danielmruales@gmail.com>
2019-05-06 23:11:42 -0600

    trying to get toggling between matches and messages to work

--- client/src/Matches/Matches.js
+++ client/src/Matches/Matches.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Matches.css'
 class Matches extends Component {
-    render() {
+    render(props) {
         return (
             <div className='mainMatchesDiv'>
                 <div className='matchesNav'>
@@ -9,7 +9,7 @@ class Matches extends Component {
                     <h1> My Profile</h1>
                 </div>
                 <div className='matches'>
-                    <div className='options'><p>Matches</p> <p>Messages</p></div>
+                    <div className='options'><p>Matches</p> <p onClick={() => props.history.push('/messages')}>Messages</p></div>
                     <div className='imgDiv'>
                         <img src='https://images.unsplash.com/photo-1553564003-28e601062b3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80' alt='matches'/>
                         <img src='https://images.unsplash.com/photo-1553564003-28e601062b3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80' alt='matches'/>
